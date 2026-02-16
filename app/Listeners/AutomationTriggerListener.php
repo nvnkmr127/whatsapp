c0123d9265ca993b3ea2a3889b11667e5048c02b
Naveen <nvnkmr127@gmail.com>
2026-02-16 21:38:16 +0530

    sddfbdf

--- app/Listeners/AutomationTriggerListener.php
+++ app/Listeners/AutomationTriggerListener.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Cache;
 
-class AutomationTriggerListener implements ShouldQueue
+class AutomationTriggerListener
 {
     use InteractsWithQueue;
 
