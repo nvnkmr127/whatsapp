b906a1020e78f9f1e36973bec3c47ae5e676f94d
Naveen <nvnkmr127@gmail.com>
2026-02-16 22:09:37 +0530

    gergegdfg

--- app/Listeners/SyncCallToInboxListener.php
+++ app/Listeners/SyncCallToInboxListener.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 
-class SyncCallToInboxListener implements ShouldQueue
+class SyncCallToInboxListener
 {
     use InteractsWithQueue;
 
