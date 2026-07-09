647464205a3ffed81eadc6d7f891bf7b8a8e8874
Naveen <nvnkmr127@gmail.com>
2026-07-09 11:39:53 +0530

    chore: remove temporary test scripts and clean up Next.js build artifacts

--- print_raw_lines.php
+++
@@ -1,5 +0,0 @@
-<?php
-$lines = explode("\n", file_get_contents('temp_viewed_25.json'));
-for ($i = 0; $i < min(15, count($lines)); $i++) {
-    echo "Line $i (len=" . strlen($lines[$i]) . "): [" . $lines[$i] . "]" . PHP_EOL;
-}
