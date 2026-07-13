4e475c6783c17c3c76bcf95ffca974a1f4d66af2
Naveen <nvnkmr127@gmail.com>
2026-07-13 14:54:14 +0530

    refactor: remove unused features, deprecated controllers, and obsolete dependencies to reduce codebase complexity

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -57,43 +57,6 @@ if (! function_exists('set_setting')) {
     }
 }
 
-if (! function_exists('t')) {
-    function t($key)
-    {
-        return __($key);
-    }
-}
-
-if (! function_exists('checkPermission')) {
-    function checkPermission($permission)
-    {
-        $permissions = is_array($permission) ? $permission : [$permission];
-        $user = auth()->user();
-
-        if (! $user) {
-            return false;
-        }
-
-        foreach ($permissions as $perm) {
-            if ($user->can($perm)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-}
-
-if (! function_exists('whatsapp_log')) {
-    function whatsapp_log($message, $level = 'info', $context = [], $exception = null)
-    {
-        if ($exception) {
-            $context['exception'] = $exception->getMessage();
-            $context['trace'] = $exception->getTraceAsString();
-        }
-        \Illuminate\Support\Facades\Log::channel('daily')->log($level, 'WA API: '.$message, $context);
-    }
-}
 if (! function_exists('audit')) {
     function audit(string $event, $userId = null, $identifier = null, ?string $provider = null, array $metadata = [])
     {
@@ -105,16 +68,5 @@ if (! function_exists('money')) {
     function money($amount, $currency = 'USD')
     {
         return Illuminate\Support\Number::currency((float) $amount, $currency);
-    }
-}
-
-if (! function_exists('flash_message')) {
-    /**
-     * Flash a message to the session for toast notifications.
-     * types: success, error, warning, info
-     */
-    function flash_message($message, $type = 'success')
-    {
-        session()->flash($type, $message);
     }
 }
