9c23ba44553850e95fdf3a578a7cbfb2e217fc1d
Naveen <nvnkmr127@gmail.com>
2026-07-21 17:12:51 +0530

    feat: include reply_to_message_id in message persistence logic

--- app/Jobs/PersistMessageJob.php
+++ app/Jobs/PersistMessageJob.php
@@ -292,6 +292,7 @@ class PersistMessageJob implements ShouldQueue
                 'media_type' => $mediaType,
                 'caption' => $caption,
                 'attributed_campaign_id' => $attributedCampaignId,
+                'reply_to_message_id' => $replyToMessageId,
             ]);
 
             // 6. Fan-out for Side Effects
